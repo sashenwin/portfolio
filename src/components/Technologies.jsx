b132f898f8577c8e6f96c982fe5bcd5f3b8e0a1e
Sashen Windsor <sashen@echonlabs.com>
2024-06-13 22:21:11 +0530

    feat: :tada: Basic structure of the webpage

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,84 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { RiAngularjsFill } from "react-icons/ri";
+import { FaHtml5 } from "react-icons/fa6";
+import { FaCss3Alt } from "react-icons/fa6";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaJava } from "react-icons/fa6";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h2 
+      whileInView={{opacity:1, y:0}}
+      initial={{opacity:0, y:-100}}
+      transition={{duration:1.5}}
+      className="my-20 text-center text-4xl">Technologies</motion.h2>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiAngularjsFill className="text-7xl text-red-700" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaHtml5 className="text-7xl text-amber-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaCss3Alt className="text-7xl text-sky-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(7)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <IoLogoJavascript className="text-7xl text-yellow-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaJava className="text-7xl" />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Technologies;
